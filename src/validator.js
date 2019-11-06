9a9069339ee9cf231efdd09f130f43b53b335373
ajimae <meeky.ae@gmail.com>
2019-11-06 14:46:57 +0100

    - added comments to source file
    [Finishes #000]

--- src/validator.js
+++ src/validator.js
@@ -1,3 +1,24 @@
+/**
+ * - split the card number into individual characters, convert each to digits(or number)
+ * - start with the right most character excluding the check digit and double each with
+ *   second or alternate digit with a factor e.g 9 in this case
+ * - if any digit is greater than the factor, subtract the factor from the digit e.g
+ *   digit > 9 then digit - 9
+ * - sum up resulting digits array, if the number is a multiple of 10, then the number
+ *   is valid else it's not a valid credit card number.
+ * 
+ *   Example
+ *   - Split into digits: 4 0 1 2 8 8 8 8 8 8 8 8 1 8 8 1.
+ *   - Double every second except the check digit, right to left: 8 0 2 2 16 8 16 8 16 8 16 8 2 8 16 1.
+ *   - Add digits of any above nine: 8 0 2 2 7 8 7 8 7 8 7 8 2 8 7 1.
+ *   - Sum the digits: 90.
+ *   - Is the sum a multiple of 10? - true.
+ * 
+ * @param {string} cardNumber 
+ * 
+ * @return {boolean} true | false
+ */
+
 function validateCreditCard(cardNumber) {
   var toDigits = cardNumber
     .replace(/[^0-9]/g, "")
